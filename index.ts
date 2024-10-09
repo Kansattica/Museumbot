76289133fa33ef65805f0b493985c873b3816d65
Grace Lovelace <grace@hypnovir.us>
2024-10-09 12:56:16 -0700

    Progress!

--- index.ts
+++ index.ts
@@ -2,6 +2,14 @@ import { BskyAgent } from '@atproto/api';
 import * as dotenv from 'dotenv';
 import { CronJob } from 'cron';
 import * as process from 'process';
+import { parse } from 'csv-parse/sync';
+import { readFileSync } from 'fs';
+
+interface Postable
+{
+	images: string[];
+	body: string;
+}
 
 dotenv.config();
 
@@ -10,17 +18,38 @@ const agent = new BskyAgent({
     service: 'https://bsky.social',
   })
 
+function get_shuffle_posts() : Postable[]
+{
+	const posts = parse(readFileSync('./posts.csv', 'utf-8'), {
+		columns: true,
+		skip_empty_lines: true
+	}).map((post) => {
+		"images": post.filename.split(" AND ");
+		"body": post.description;
+	});
+
+	return posts;
+}
 
 async function main() {
+
+	const posts_to_make = get_shuffle_posts();
+
+	console.log(posts_to_make);
+
+	return;
+	
+
     await agent.login({ identifier: process.env.BLUESKY_USERNAME!, password: process.env.BLUESKY_PASSWORD!})
     await agent.post({
-        text: "test time for you 🦨"
+        text: "test time for you 🦨",
+		langs: ["en-US"],
+		createdAt: new Date().toISOString()
     });
     console.log("Just posted!")
 }
 
 main();
-
 
 // Run this on a cron job
 //const scheduleExpressionMinute = '* * * * *'; // Run once every minute for testing
