659be36c679f560818b07fe27b134ae374661a84
Grace Lovelace <grace@hypnovir.us>
2024-10-09 13:56:10 -0700

    Makin' progress!

--- index.ts
+++ index.ts
@@ -5,10 +5,21 @@ import * as process from 'process';
 import { parse } from 'csv-parse/sync';
 import { readFileSync } from 'fs';
 
-interface Postable
+interface CsvRow
+{
+	filename: string;
+	description: string;
+}
+
+class Postable
 {
 	images: string[];
 	body: string;
+
+	constructor(filenames: string[], description: string) {
+		this.images = filenames;
+		this.body = description;
+	}
 }
 
 dotenv.config();
@@ -18,31 +29,59 @@ const agent = new BskyAgent({
     service: 'https://bsky.social',
   })
 
+function getRandomInt(min: number, max: number): number {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+}
+
+function shuffle(list: any[]) : any[]
+{
+	for (let i = 0; i < list.length - 1; i++)
+	{
+		const pivot = getRandomInt(i, list.length);
+		[list[i], list[pivot]] = [list[pivot], list[i]];
+	}
+
+	return list;
+}
+
 function get_shuffle_posts() : Postable[]
 {
-	const posts = parse(readFileSync('./posts.csv', 'utf-8'), {
+	return shuffle(parse(readFileSync('./posts.csv', 'utf-8'), {
 		columns: true,
 		skip_empty_lines: true
-	}).map((post) => {
-		"images": post.filename.split(" AND ");
-		"body": post.description;
-	});
+	}).map((post: CsvRow) => new Postable(post.filename.split(" AND "), post.description)));
+}
 
-	return posts;
+class ShuffleState
+{
+	private static posts: Postable[] = get_shuffle_posts();
+
+	static get_next_post(): Postable
+	{
+		if (ShuffleState.posts.length === 0)
+		{
+			ShuffleState.posts = get_shuffle_posts();
+			console.log("Reshufflin' over here!");
+		}
+
+		// we know this can't be undefined
+		return ShuffleState.posts.pop() as Postable;
+	}
 }
 
 async function main() {
 
-	const posts_to_make = get_shuffle_posts();
+	const post = ShuffleState.get_next_post();
 
-	console.log(posts_to_make);
+	console.log(post);
 
 	return;
-	
 
     await agent.login({ identifier: process.env.BLUESKY_USERNAME!, password: process.env.BLUESKY_PASSWORD!})
     await agent.post({
-        text: "test time for you 🦨",
+        text: post.body,
 		langs: ["en-US"],
 		createdAt: new Date().toISOString()
     });
@@ -55,6 +94,6 @@ main();
 //const scheduleExpressionMinute = '* * * * *'; // Run once every minute for testing
 //const scheduleExpression = '0 */3 * * *'; // Run once every three hours in prod
 
-//const job = new CronJob(scheduleExpression, main); // change to scheduleExpressionMinute for testing
+//const job = new CronJob(scheduleExpressionMinute, main); // change to scheduleExpressionMinute for testing
 
 //job.start();
